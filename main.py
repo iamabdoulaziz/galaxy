11cfc22b2de22fe2be6470c5c743a826d0493edd
Cute Boy <abdoulazizc867@gmail.com>
2025-07-04 13:33:57 +0000

    move forward the tile

--- main.py
+++ main.py
@@ -35,6 +35,8 @@ class MainWidget(Widget):
 
     current_speed_x = 0
 
+    current_y_loop = 0
+
     tile = None
     ti_x = 0
     ti_y = 0
@@ -86,6 +88,7 @@ class MainWidget(Widget):
         return line_y
 
     def get_tile_coordinates(self, ti_x, ti_y):
+        ti_y = ti_y - self.current_y_loop
         x = self.get_line_x_from_index(ti_x)
         y = self.get_line_y_from_index(ti_y)
         return x, y
@@ -139,14 +142,15 @@ class MainWidget(Widget):
         self.update_vertical_lines()
         self.update_horizontal_lines()
         self.update_tiles()
-        # self.current_offset_y += self.SPEED * time_factor
+        self.current_offset_y += self.SPEED * time_factor
 
 
         spacing_y = self.H_LINES_SPACING * self.height
         if self.current_offset_y >= spacing_y:
             self.current_offset_y -= spacing_y
+            self.current_y_loop += 1
 
-        # self.current_offset_x += self.current_speed_x * time_factor
+        self.current_offset_x += self.current_speed_x * time_factor
 
 
 class GalaxyApp(App):
