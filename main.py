cbe8cb40f98236be202192d8c98774a72de2da59
Cute Boy <abdoulazizc867@gmail.com>
2025-07-07 19:02:40 +0000

    State game over

--- main.py
+++ main.py
@@ -48,6 +48,8 @@ class MainWidget(Widget):
     ship = None
     ship_coordinates = [(0, 0), (0, 0), (0, 0)]
 
+    state_game_over = False
+
     def __init__(self, **kwargs):
         super(MainWidget, self).__init__(**kwargs)
         #print(f"INIT W: {str(self.width)} H: {str(self.height)}")
@@ -240,20 +242,23 @@ class MainWidget(Widget):
         self.update_horizontal_lines()
         self.update_tiles()
         self.update_ship()
-        speed_y = self.SPEED * self.height / 100
-        self.current_offset_y += speed_y * time_factor
+
+        if not self.state_game_over:
+            speed_y = self.SPEED * self.height / 100
+            self.current_offset_y += speed_y * time_factor
 
 
-        spacing_y = self.H_LINES_SPACING * self.height
-        if self.current_offset_y >= spacing_y:
-            self.current_offset_y -= spacing_y
-            self.current_y_loop += 1
-            self.generate_tiles_coordinates()
+            spacing_y = self.H_LINES_SPACING * self.height
+            while self.current_offset_y >= spacing_y:
+                self.current_offset_y -= spacing_y
+                self.current_y_loop += 1
+                self.generate_tiles_coordinates()
 
-        speed_x = self.current_speed_x * self.width/100
-        self.current_offset_x += speed_x * time_factor
+            speed_x = self.current_speed_x * self.width/100
+            self.current_offset_x += speed_x * time_factor
 
-        if not self.check_ship_collisions():
+        if not self.check_ship_collisions() and not self.state_game_over:
+            self.state_game_over = True
             print("GAME OVER")
 
 class GalaxyApp(App):
